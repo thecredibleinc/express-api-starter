6d4d71329e9f01c55701209a33e28ca2d5dddc7f
amn <satija.aman@gmail.com>
2022-08-30 03:27:17 +0530

    added proper error middleware

--- src/api/common/middlewares/error.middleware.js
+++ src/api/common/middlewares/error.middleware.js
@@ -1,21 +1,74 @@
-function notFound(req, res, next) {
-  res.status(404);
-  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
-  next(error);
-}
+import HttpStatus from 'http-status-codes';
 
-/* eslint-disable no-unused-vars */
-function errorHandler(err, req, res, next) {
-  /* eslint-enable no-unused-vars */
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+import logger from '../../../utils/logger';
+import buildError from '../../../utils/build_error';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import logger from '../../../utils/logger';
+import { constents } from '../configs/constents.config';
+import ResponseFormatter from '../../../utils/responseFormatter.util';
+
+/**
+ * Error response middleware for 404 not found.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ */
+export function notFound(req, res) {
+  // console.log(res)
+  res.status(HttpStatus.NOT_FOUND).json({
+      code: HttpStatus.NOT_FOUND,
+      message: HttpStatus.getStatusText(HttpStatus.NOT_FOUND),
+      error: HttpStatus.getStatusText(HttpStatus.NOT_FOUND),
+      data:{}
   });
 }
 
-module.exports = {
-  notFound,
-  errorHandler
-};
+/**
+ * Method not allowed error middleware. This middleware should be placed at
+ * the very bottom of the middleware stack.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ */
+export function methodNotAllowed(req, res) {
+  res.status(HttpStatus.METHOD_NOT_ALLOWED).json({
+      code: HttpStatus.METHOD_NOT_ALLOWED,
+      message: HttpStatus.getStatusText(HttpStatus.METHOD_NOT_ALLOWED),
+      error: HttpStatus.getStatusText(HttpStatus.METHOD_NOT_ALLOWED),
+      data:{}
+  });
+}
+
+/**
+ * To handle errors from body parser for cases such as invalid JSON sent through
+ * the body (https://github.com/expressjs/body-parser#errors).
+ *
+ * @param  {Object}   err
+ * @param  {Object}   req
+ * @param  {Object}   res
+ * @param  {Function} next
+ */
+export const bodyParser = async (err, req, res, next) => {
+  await logIt(constents.log_levels.list.ERROR, `${err.message}: ${err}`, req, res);
+
+  res.status(err.status).json({
+      code: err.status,
+      message: HttpStatus.getStatusText(err.status),
+      error: HttpStatus.getStatusText(err.status),
+      data:{}
+  });
+}
+
+/**
+ * Generic error response middleware for validation and internal server errors.
+ *
+ * @param  {Object}   err
+ * @param  {Object}   req
+ * @param  {Object}   res
+ * @param  {Function} next
+ */
+export const genericErrorHandler = async (err, req, res, next) => {
+  await logger.info(constents.log_levels.list.ERROR, `${err.message}: ${err}`, req, res);
+  const error = buildError(err);
+  res.status(error.code).send(ResponseFormatter.format(err,err.code))
+}
