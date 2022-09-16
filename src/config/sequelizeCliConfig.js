c8431fde2cce80be03926dc058b3e0bbfa90850d
amn <satija.aman@gmail.com>
2022-09-17 02:36:07 +0530

    improved security +scripts + naming related db migrations

---
+++ src/config/sequelizeCliConfig.js
@@ -0,0 +1,36 @@
+import './config'
+
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CLIENT,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CLIENT,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_CLIENT,
+    // dialectOptions: {
+    //   bigNumberStrings: true,
+    //   ssl: {
+    //     ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+    //   }
+    // }
+  }
+};
